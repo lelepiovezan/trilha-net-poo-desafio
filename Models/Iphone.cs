d0c8d9bbdc38609ffadf39828b3d1387a9c3f656
Leandro <leandro.piovezan@yahoo.com.br>
2023-10-26 21:18:34 -0300

    Add files via upload

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+   
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string _modelo, string _imei, int _memoria) : base(numero, _modelo, _imei, _memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o app {nomeApp} no Iphone");
+        }
     }
 }
