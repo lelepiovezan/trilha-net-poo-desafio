07399fbae407ebf09892c30d7022e7f7784ae0af
Leandro <leandro.piovezan@yahoo.com.br>
2023-10-26 21:19:56 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia n1 = new Nokia("1234-5678","Nokia","234323432423424",256);
+n1.Ligar();
+n1.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone i1 = new Iphone("3454-3455","Iphone","2343243243223",512);
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Telgran");
